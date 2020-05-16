2ba86972b10473668810e3d916ec7151e32805ee
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-16 19:30:47 +0300

    wip: Added react-icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,83 @@
 import React from "react"
 import "../styles.css"
-
-export default () => <div className="bg-blue-200">Hello world!</div>
+import { FaDumbbell, FaRunning, FaBatteryQuarter, FaBed } from "react-icons/fa"
+export default () => {
+  return (
+    <div className="container">
+      <div className="bg1">
+        <h2>
+          16 <span>| 24</span>
+        </h2>
+        <p>Goals Completed</p>
+      </div>
+      <div className="bg1">
+        <h2>
+          <FaBatteryQuarter />
+          {/* <i className="fas fa-battery-three-quarters"></i> */}
+        </h2>
+        <p>Respiration</p>
+      </div>
+      <div className="bg2">
+        <h2>
+          <FaRunning />
+          {/* <i className="fas fa-running"></i> */}
+        </h2>
+        <p>Miles</p>
+      </div>
+      <div className="bg1">
+        <h2>36 &deg;</h2>
+        <p>Temperature</p>
+      </div>
+      <div className="bg1">
+        <h2>
+          <FaBed />
+          {/* <i className="fas fa-bed"></i> */}
+        </h2>
+        <p>Sleep Keep</p>
+      </div>
+      <div className="bg2">
+        <h2>
+          98 <span>bpm</span>
+        </h2>
+        <p>Heart Rate</p>
+      </div>
+      <div className="bg1">
+        <h2>
+          170 <span>lbs</span>
+        </h2>
+        <p>Weight</p>
+      </div>
+      <div className="bg1">
+        <h2>
+          28 <span>%</span>
+        </h2>
+        <p>Fat Percentage</p>
+      </div>
+      <div className="bg2">
+        <h2>
+          118 <span>mgdl</span>
+        </h2>
+        <p>Blood Glucose</p>
+      </div>
+      <div className="bg2">
+        <h2>
+          680 <span>kcal</span>
+        </h2>
+        <p>AVG Consumption</p>
+      </div>
+      <div className="bg2">
+        <h2>
+          <FaDumbbell />
+          {/* <i className="fas fa-dumbbell"></i> */}
+        </h2>
+        <p>Workouts</p>
+      </div>
+      <div className="bg2">
+        <h2>
+          85 <span>%</span>
+        </h2>
+        <p>Body Hydration</p>
+      </div>
+    </div>
+  )
+}
