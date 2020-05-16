9502d309325e2189c88991f35a676b4c1f9fd0eb
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-16 20:28:29 +0300

    wip: Added some styling to icons and headings

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,77 +4,77 @@ import { FaDumbbell, FaRunning, FaBatteryQuarter, FaBed } from "react-icons/fa"
 import bg01 from "../images/sunset.jpg"
 export default () => {
   return (
-    <div className="con w-full grid grid-rows-4 grid-cols-4 gap-2">
-      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-32 col-span-2">
-        <h2>
+    <div className="con w-full grid grid-rows-4 grid-cols-4 gap-1 h-full">
+      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform col-span-2">
+        <h2 className="text-3xl">
           16 <span>| 24</span>
         </h2>
         <p>Goals Completed</p>
       </div>
-      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-32 ">
+      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform ">
         <h2>
-          <FaBatteryQuarter />
+          <FaBatteryQuarter className="text-3xl" />
           {/* <i className="fas fa-battery-three-quarters"></i> */}
         </h2>
         <p>Respiration</p>
       </div>
-      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-32">
+      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
         <h2>
-          <FaRunning />
+          <FaRunning className="text-3xl" />
           {/* <i className="fas fa-running"></i> */}
         </h2>
         <p>Miles</p>
       </div>
-      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-32">
-        <h2>36 &deg;</h2>
+      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
+        <h2 className="text-3xl">36 &deg;</h2>
         <p>Temperature</p>
       </div>
-      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-32">
+      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
         <h2>
-          <FaBed />
+          <FaBed className="text-3xl" />
           {/* <i className="fas fa-bed"></i> */}
         </h2>
         <p>Sleep Keep</p>
       </div>
-      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-32 col-span-2">
-        <h2>
+      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform col-span-2">
+        <h2 className="text-3xl">
           98 <span>bpm</span>
         </h2>
         <p>Heart Rate</p>
       </div>
-      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-32">
-        <h2>
+      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
+        <h2 className="text-3xl">
           170 <span>lbs</span>
         </h2>
         <p>Weight</p>
       </div>
-      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-32">
-        <h2>
+      <div className="bg1 bg-red-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
+        <h2 className="text-3xl">
           28 <span>%</span>
         </h2>
         <p>Fat Percentage</p>
       </div>
-      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-32 col-span-2">
-        <h2>
+      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform col-span-2">
+        <h2 className="text-3xl">
           118 <span>mgdl</span>
         </h2>
         <p>Blood Glucose</p>
       </div>
-      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-32 col-span-2">
-        <h2>
+      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform col-span-2">
+        <h2 className="text-3xl">
           680 <span>kcal</span>
         </h2>
         <p>AVG Consumption</p>
       </div>
-      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-32">
+      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
         <h2>
-          <FaDumbbell />
+          <FaDumbbell className="text-3xl" />
           {/* <i className="fas fa-dumbbell"></i> */}
         </h2>
         <p>Workouts</p>
       </div>
-      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-32">
-        <h2>
+      <div className="bg2 bg-blue-200 flex items-center justify-center flex-col h-40 hover:scale-95 transform">
+        <h2 className="text-3xl">
           85 <span>%</span>
         </h2>
         <p>Body Hydration</p>
