1b10940137206c3f13ac0548ae0bc378d7f047e1
TheNomanAhmed <nomanahmed478@gmail.com>
2020-05-18 08:39:34 +0300

    wip: MDX Blog updated

---
+++ src/components/Cta.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export const Cta = ({ line1, line2, btn1, btn2, link1, link2 }) => {
+  return (
+    <div className="bg-background mt-12 pb-8">
+      <div className=" mx-auto">
+        <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+          {line1}
+          <br />
+          <span className="text-indigo-600">{line2}</span>
+        </h2>
+        <div className="mt-8 flex item-center flex0">
+          <div className="inline-flex rounded-md shadow">
+            <Link
+              to={link1}
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+            >
+              {btn1}
+            </Link>
+          </div>
+          <div className="ml-3 inline-flex rounded-md shadow">
+            <Link
+              to={link2}
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+            >
+              {btn2}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 import { useTheme } from "../utils/themeContext"
-import { Header, Button, InfoCardComp } from "./index"
+import { Header, Button, InfoCardComp, Cta } from "./index"
 
-const shortcodes = { Button, InfoCardComp }
+const shortcodes = { Button, Cta, InfoCardComp }
 
 const Layout = ({ children }) => {
   const { theme } = useTheme()
